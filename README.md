9906da54e00e7101ba2b8488d6223837447ad5de
rohan <rohannair2939@gmail.com>
2024-01-31 21:11:22 +0530

    workflow added

---
+++ README.md
@@ -0,0 +1,5 @@
+# customer_satisfaction
+Predicting how a customer feels about a product before they even ordered it
+ 
+
+ zenml does caching process basically if no chages done it performs from previous runs only
---
+++ run_pipeline.py
@@ -0,0 +1,3 @@
+from pipelines.training_pipeline import train_pipeline
+if __name__ == "__main__":
+    train_pipeline(data_path='/Users/rohansridhar/Desktop/customer_satisfaction/data/olist_customers_dataset.csv')
